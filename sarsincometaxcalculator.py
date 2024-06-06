144efc09d410d9cc6347b2805330ff6801432921
oniisan-rakki <153196954+oniisan-rakki@users.noreply.github.com>
2024-06-06 16:16:53 +0200

    Create sarsincometaxcalculator.py

---
+++ sarsincometaxcalculator.py
@@ -0,0 +1,13 @@
+#functions
+def tax_caluclator():
+    pass
+print('Hey there!\n Welcome to the income tax calculator.')
+tax_year = []
+year = input('Which tax year would you like to calculate? ')
+print (tax_year)
+age = input('What is your age? ')
+change = input('Do you receive the same income every month? (Yes/No) ')
+if change == 'Yes':
+    pass
+else:
+    pass
